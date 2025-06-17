15b132f350b7d6b181d27f90dcbb4b3baff9958c
María Bueno Álvez <buenoalvezm@gmail.com>
2025-06-17 14:09:09 +0200

    add mapping in HT data

--- scripts/assay_exploration/prep_mapping_olink_uniprot.R
+++ scripts/assay_exploration/prep_mapping_olink_uniprot.R
@@ -7,3 +7,10 @@ resource_data <- read_csv("data/processed/final_data/data_resource_20240604.csv"
 resource_data |> 
   distinct(Assay, UniProt) |> 
   write_tsv("data/olink_uniprot_mapping.tsv")
+
+# For HR data
+data_bamse <- read_tsv("data/final_data/HPA/v24_2/bamse_data_ht_phase2.tsv")
+
+data_bamse |> 
+  distinct(Assay, OlinkID, UniProt) |> 
+  write_tsv("data/olink_uniprot_mapping_ht.tsv")
